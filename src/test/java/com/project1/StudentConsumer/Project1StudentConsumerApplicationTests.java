ecb2b5ab7c634e48d336e37321c8ba68dd5011a3
Bhoomika-NV <bhoomikanv6@gmail.com>
2024-05-29 21:52:00 +0530

    build one

---
+++ src/test/java/com/project1/StudentConsumer/Project1StudentConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project1.StudentConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1StudentConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
